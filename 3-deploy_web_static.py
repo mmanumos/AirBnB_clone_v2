698cb9f343720c7e2a8f54ec5c5d35c4b2a43607
marcewp15 <970@holbertonschool.com>
2020-04-12 02:34:25 +0000

    This script creates and distributes an archive to your web servers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -23,7 +23,7 @@ def do_pack():
         now = datetime.now()
         date_create = now.strftime("%Y%m%d%H%M%S")
         local("mkdir -p versions")
-        do_tgz = "web_static_{}.tgz".format(date_create)
+        do_tgz = "versions/web_static_{}.tgz".format(date_create)
         local("tar -cvzf versions/{} web_static".format(do_tgz))
         return do_tgz
     except:
