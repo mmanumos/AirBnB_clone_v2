8f43be2f02e8b2a0ea9348f4987b608782548423
marcewp15 <970@holbertonschool.com>
2020-04-12 01:28:59 +0000

    Fix pep8 style, This script distributes an archive to the web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,7 +11,7 @@ env.hosts = ['35.231.24.237', '54.90.204.128']
 
 
 def do_pack():
-    """ Function that makes a packs """
+    """ Function that makes packages"""
     try:
         now = datetime.now()
         date_create = now.strftime("%Y%m%d%H%M%S")
@@ -24,6 +24,7 @@ def do_pack():
 
 
 def do_deploy(archive_path):
+    """ distributes an archive to a web server """
     if os.path.exists(archive_path) is False:
         return False
     try:
@@ -31,12 +32,15 @@ def do_deploy(archive_path):
         a = path_id[1].split('.')
         put(archive_path, "/tmp")
         run("mkdir -p /data/web_static/releases/{}".format(a[0]))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(path_id[1], a[0]))
+        run("tar -xzf /tmp/{} -C\
+        /data/web_static/releases/{}".format(path_id[1], a[0]))
         run("rm /tmp/{}".format(path_id[1]))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(a[0], a[0]))
+        run("mv /data/web_static/releases/{}/web_static/*\
+        /data/web_static/releases/{}".format(a[0], a[0]))
         run("rm -rf /data/web_static/releases/{}/web_static".format(a[0]))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(a[0]))
+        run("ln -s /data/web_static/releases/{}/\
+        /data/web_static/current".format(a[0]))
         print("New version deployed!")
         return True
     except:
