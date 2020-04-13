20deb114d265de89f395571d5010ec3c8331ff91
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-13 01:38:03 +0000

    [Feature] The server is setted for the deployment of web_static. modified:   0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
-# Sets up your web servers for the deployment of web_static
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "Fake index.html web-01" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Bash Scripot to set up web servers for web_static deployment
+sudo apt-get install update
+sudo apt-get install -y nginx
+
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+
+echo "Simple content" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "40i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+
+sudo sed -i '39 i\   \tlocation /hbnb_static {\n\talias /data/web_static/current;\n}' /etc/nginx/sites-available/default
+
 sudo service nginx restart
